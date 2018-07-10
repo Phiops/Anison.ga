e52c92cf5f3d4db9d5a183ecfd927c3f912bacc0
PhWill <phiopsneferkare@gmail.com>
2018-07-11 01:16:08 +0200

    Added some responsive behavior for xs screens.

--- resources/views/requests.blade.php
+++ resources/views/requests.blade.php
@@ -9,14 +9,38 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount)
 <head>
     @include('inc.jquerybs')
     <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <title>Requests - {{setting('site.title')}}</title>
 </head>
 
 @include('inc.navbar')
-<br><body style="text-align: center;">
-    <h1>Requested songs</h1>
+<br><body style="text-align: left;">
+
+
+    <section class="py-5">
+        <div class="container">
+            <h1 class="mb-3"><i class="fas fa-parachute-box"></i> Drop a request</h1>
+            <div class="card">
+                <div class="card-body">
+            @if(count($errors) > 0)@foreach($errors->all() as $error)
+                <div class="alert alert-danger"><br>{{$error}}<br></div>
+            @endforeach @endif
+
+            {!! Form::open(['url' => 'requests/new']) !!}
+            <div class="form-group">{{Form::label('song', 'Song')}} {{Form::text('song', '', ['class' => 'form-control', 'placeholder' => 'Enter song'])}}</div>
+            <div class="form-group">{{Form::label('anime', 'Anime')}} {{Form::text('anime', '', ['class' => 'form-control', 'placeholder' => 'Enter anime title'])}}</div>
+            <div class="form-group">{{Form::label('link', 'Link')}} {{Form::text('link', '', ['class' => 'form-control', 'placeholder' => 'Enter a URL that links to this song (e.g. YouTube, Spotify, CDJapan, ...)'])}}</div>
+            <div>{{Form::submit('Submit', ['class' => 'btn btn-block btn-lg btn-outline-primary'])}}</div>
+            {!! Form::close() !!}
+        </div>
+</div>
+</div>
+    </section>
+
+    
     <section class='py-5'>
         <div class='container'>
+            <h1 class="mb-3"><i class="fas fa-box-open"></i> Requested songs</h1>
             <div class="progress">
                 <div class="progress-bar" role="progressbar" style="width: <?= $percentage;?>%" aria-valuenow="<?= $percentage;?>" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
--- resources/views/songs/all.blade.php
+++ resources/views/songs/all.blade.php
@@ -41,7 +41,7 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
                 <div class="card top-card shadow-lg">
                     <div class="row">
                         <?php $latestsong = Song::latest()->first(); include 'anilist.php';?>
-                        <div class="col-lg-4 anime-img-bg" style="background-image: url({{$image}})">
+                        <div class="col-lg-4 col-12 anime-img-bg" style="background-image: url({{$image}})">
                             <div class="row">
                             <span class="ml-3 p-2 badge badge-warning">New</span>
                             </div>
@@ -51,14 +51,14 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
                                 </span>
                             </div>
                         </div>
-                        <div class="col-lg-6">
+                        <div class="col-lg-6 col-12">
                             <div class="p-2">
                                 <h3 class="card-title">{{ $latestsong->song }} </h3>
                                 <p> @if(empty($latestsong->artistid)){{$latestsong->artist}} @else <a target="_blank" href='https://anilist.co/staff/{{$latestsong->artistid}}'>{{$latestsong->artist}}</a> @endif
                     </p>
                             </div>
                         </div>
-                        <div class="col-lg-2 my-play-button">
+                        <div class="col-lg-2 col-sm-12 my-play-button">
                             <button type="button" class="btn btn-dark" onclick="location.href = '/play/{{$latestsong->id}}';">
                                 <i class="fas fa-play"></i>
                                 </button>
@@ -67,7 +67,7 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
                     
                 </div>
 
-                <div class="mt-lg-3 card top-card shadow-lg">
+                <div class="mt-lg-3 mt-4 card top-card shadow-lg">
                     <div class="row">
                         <?php $latestsong = Song::orderBy('id', 'desc')->skip(1)->take(1)->get()->first(); include 'anilist.php';?>
                         <div class="col-lg-4 anime-img-bg" style="background-image: url({{$image}})">
@@ -100,7 +100,7 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
 
             <!-- SEARCH CARD ON RIGHT HAND SIDE -->
 
-            <div class="col-xl-4">
+            <div class="col-xl-4 col-sm-12 mt-4 mt-md-0">
                 <div class="card shadow-lg">
                     <div class="card-body"><div class"card-title mb-5">
                     <h5>
@@ -125,20 +125,20 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
      
 <!-- RECENT ENTRIES ADDED TO DATABASE -->
 
-    <div class="container  pt-lg-5 pb-5">
+    <div class="container pt-md-5 pt-lg-5 pb-5 pt-4">
     <div class="row pb-3 pl-3">
         <h2>
             <i class="fas fa-database"></i> Recently added songs</h2>
     </div>
     <div class="progress"><div class="progress-bar" role="progressbar" style="width: <?= $percentage;?>%" aria-valuenow="<?= $percentage;?>" aria-valuemin="0" aria-valuemax="100"></div></div>
      
-    <div class="row d-flex">
+    <div class="row">
         <div class="col-xl-12">
             
-            <div class="card top-card shadow">
+            <div class="card list-card shadow">
                 <div class="p-2">Pick your poison, our database is stuffed. </div>
                 <ul class="list-group list-group-flush">
-                <li class="list-group-item">
+                <li class="list-group-item mobile-blank">
                     <div class="row">
                         <div class="col col-heading">Anime</div>
                         <div class="col col-heading">Song</div>
@@ -148,22 +148,25 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
                 </li>    
                 @if(count($song) > 0) @foreach($song as $song)
                     <li class="list-group-item">
-                        <div class="row">
-                            <div class="col">
+                        <div class"card pb-1">
+                            <div class="card-body">
+                        <div class="row row-cap">
+                            <div class="col-md-3 col-sm-12">
                             <a target="_blank" href='https://anilist.co/anime/{{$song->animeid}}'>{{$song->anime}}</a>
                             </div>
-                            <div class="col">
+                            <div class="col-md-3 col-sm-12 song-title-sm">
                             <a href="/play/{{$song->id}}">{{$song->song}}</a>
                             </div>
-                            <div class="col">
+                            <div class="col-md-3 col-sm-12">
                             @if(empty($song->artistid)){{$song->artist}} @else <a target="_blank" href='https://anilist.co/staff/{{$song->artistid}}'>{{$song->artist}}</a> @endif
                             </div>
-                            <div class="col-lg-1 my-play-button">
+                            <div class="col-lg-1  col-sm-12 my-play-button play-button-cap">
                                 <button type="button" class="btn btn-dark" onclick="location.href = '/play/{{$song->id}}';">
                                     <i class="fas fa-arrow-alt-circle-right"></i>
                                 </button>
                             </div>
-                        </div>
+                        </div></div>
+                            </div>
                     </li>@endforeach @endif
                             </ul>
                                 </div>
@@ -176,13 +179,15 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
         <form><input class="round" id='myInput3' onkeyup='myFunction3()' placeholder='Search for artists..' type='text'></form>  -->
        
     <div class="container  pb-5 mt-5">
-        <div class="row pb-3 pl-3">
+        <div class="row pb-3">
             <h2>
                 <i class="fas fa-fire"></i> Most played 24h</h2>
         </div>
+        <div class="row">
         <div class="card-deck">
+
             <div class="card shadow-lg">
-                <img class="card-img-top" src="vnl1baU.png.jpg" alt="Card image cap">
+                <img class="card-img-top card-img-cap" src="vnl1baU.png.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Song title</h5>
                     <p class="card-text">Artist:</p>
@@ -206,7 +211,33 @@ $percentage = ($songamount * 100) / ($songamount + $requestamount);
                     <i class="fas fa-play"></i>
                 </button>
             </div>
+            <div class="card shadow-lg">
+                <img class="card-img-top card-img-cap" src="vnl1baU.png.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Song title</h5>
+                    <p class="card-text">Artist:</p>
+                    <p class="card-text">Anime:</p>
+                    <div class="row">
+                        <div class="col">
+                            <p class="card-text">
+                                <small class="text-muted">
+                                    <i class="fas fa-redo"></i> replayed: 837</small>
+                            </p>
+                        </div>
+                        <div class="col">
+                            <p class="card-text">
+                                <small class="text-muted">
+                                    <i class="fas fa-stopwatch"></i> duration: 3:37</small>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                </div>
-   
+                <button type="button" class="btn btn-dark my-play-button-low">
+                    <i class="fas fa-play"></i>
+                </button>
+            </div>
+            <div class="w-100 d-none d-md-block d-lg-none"><!-- wrap every 3 on md--></div>
+        </div>
+        </div>
+    </div>
 </section>
